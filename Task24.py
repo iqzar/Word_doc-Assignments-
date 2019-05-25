47e4eb3fe51a20569d9aafe890eea1e863af2b02
iqzar <50322762+iqzar@users.noreply.github.com>
2019-05-25 00:29:45 -0700

    calculate sum of first n positive integers

---
+++ Task24.py
@@ -0,0 +1,16 @@
+#python program to calculate sum of first n positive integers:
+import random
+n = int(input('enter a number: '))
+sum = 0
+for i in range (1,n+1):
+    sum =sum+i
+print(sum)    
+
+
+
+
+
+
+
+ 
+
